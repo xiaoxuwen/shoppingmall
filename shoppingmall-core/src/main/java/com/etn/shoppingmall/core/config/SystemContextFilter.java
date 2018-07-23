5061163fb489c2c58bfb49d4db4254c770c47b74
xiaoxuwen <380481791@qq.com>
2018-07-23 18:04:56 +0800

    shoppingmall-core:定义分页过滤器

--- shoppingmall-core/src/main/java/com/etn/shoppingmall/core/config/SystemContextFilter.java
+++ shoppingmall-core/src/main/java/com/etn/shoppingmall/core/config/SystemContextFilter.java
@@ -4,19 +4,27 @@ package com.etn.shoppingmall.core.config;
 import com.etn.shoppingmall.core.model.SystemContext;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
  * 分页信息过滤器
  */
+@WebFilter(urlPatterns = "/*")
 public class SystemContextFilter implements Filter {
     private Integer pageSize;
 
-    public void destroy() {
-
+    @Override
+    public void init(FilterConfig cfg) throws ServletException {
+        try {
+            pageSize = Integer.parseInt(cfg.getInitParameter("pageSize"));
+        } catch (NumberFormatException e) {
+            pageSize = 15;
+        }
     }
 
+    @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         Integer offset = 0;
         try {
@@ -39,12 +47,10 @@ public class SystemContextFilter implements Filter {
         }
     }
 
-    public void init(FilterConfig cfg) throws ServletException {
-        try {
-            pageSize = Integer.parseInt(cfg.getInitParameter("pageSize"));
-        } catch (NumberFormatException e) {
-            pageSize = 15;
-        }
+    @Override
+    public void destroy() {
     }
+
+
 
 }
