a14361b684ec6c11dd04e8326e47846e6ffd044a
xiaoxuwen <380481791@qq.com>
2018-07-24 17:03:54 +0800

    shoppingmall-common:接口返回信息类

--- shoppingmall-common/src/main/java/com/etn/shoppingmall/common/util/ResponseUtil.java
+++ shoppingmall-common/src/main/java/com/etn/shoppingmall/common/util/ResponseUtil.java
@@ -1,64 +1,98 @@
 package com.etn.shoppingmall.common.util;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ResponseUtil {
-    public static Object ok() {
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("errcode", 0);
-        obj.put("errmsg", "成功");
-        return obj;
+    private int result;         //0表示失败 1表示成功
+    private String msg;         //提示信息
+    private Object data;        //附加对象，用来存储一些特定的返回信息
+
+    public int getResult() {
+        return result;
     }
 
-    public static Object ok(Object data) {
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("errcode", 0);
-        obj.put("errmsg", "成功");
-        obj.put("data", data);
-        return obj;
+    public void setResult(int result) {
+        this.result = result;
     }
 
-    public static Object ok(String errmsg, Object data) {
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("errcode", 0);
-        obj.put("errmsg", errmsg);
-        obj.put("data", data);
-        return obj;
+    public String getMsg() {
+        return msg;
     }
 
-    public static Object fail() {
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("errcode", -1);
-        obj.put("errmsg", "错误");
-        return obj;
+    public void setMsg(String msg) {
+        this.msg = msg;
     }
 
-    public static Object fail(int errcode, String errmsg) {
-        Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("errcode", errcode);
-        obj.put("errmsg", errmsg);
-        return obj;
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public ResponseUtil() {
+        result = 1;
+    }
+
+    public ResponseUtil(int result) {
+        super();
+        this.result = result;
+    }
+
+    public ResponseUtil(int result, String msg) {
+        super();
+        this.result = result;
+        this.msg = msg;
+    }
+
+    public ResponseUtil(int result, Object data) {
+        this.result = result;
+        this.data = data;
+    }
+
+    public ResponseUtil(int result, String msg, Object data) {
+        this.result = result;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static ResponseUtil ok() {
+        return new ResponseUtil();
+    }
+
+    public static ResponseUtil ok(Object data) {
+        return new ResponseUtil(1, "成功", data);
+    }
+
+    public static ResponseUtil ok(String msg, Object data) {
+        return new ResponseUtil(1, msg, data);
+    }
+
+    public static ResponseUtil fail() {
+        return new ResponseUtil(-1, "错误");
+    }
+
+    public static ResponseUtil fail(int result, String msg) {
+        return new ResponseUtil(result, msg);
     }
 
 
-    public static Object badArgument() {
+    public static ResponseUtil badArgument() {
         return fail(401, "参数不对");
     }
 
-    public static Object badArgumentValue() {
+    public static ResponseUtil badArgumentValue() {
         return fail(402, "参数值不对");
     }
 
-    public static Object unlogin() {
-        return fail(501, "请登录");
+    public static ResponseUtil unlogin() {
+        return fail(501, "未登录");
     }
 
-    public static Object serious() {
+    public static ResponseUtil serious() {
         return fail(502, "系统内部错误");
     }
 
-    public static Object unsupport() {
+    public static ResponseUtil unsupport() {
         return fail(503, "业务不支持");
     }
 }
