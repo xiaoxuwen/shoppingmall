b5344a926189962a5cd87f9f7c3c01fbc55a85ab
e <380481791@qq.com>
2018-08-16 09:20:35 +0800

    shoppingmall-admin：产品修改功能

--- shoppingmall-wx-api/src/main/java/com/etn/shoppingmall/wx/Application.java
+++ shoppingmall-wx-api/src/main/java/com/etn/shoppingmall/wx/Application.java
@@ -2,10 +2,12 @@ package com.etn.shoppingmall.wx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication(scanBasePackages = {"com.etn.shoppingmall.wx","com.etn.shoppingmall.common", "com.etn.shoppingmall.core"})
 @MapperScan("com.etn.shoppingmall.core.mapper")
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
